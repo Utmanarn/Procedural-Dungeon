af57d943019d70ea5ae2865f62c738e2ddf03bcd
Utmanarn <Utmanarn@gmail.com>
2024-05-02 14:59:44 +0200

    Update .idea

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float _movementSpeed;
+    private 
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
